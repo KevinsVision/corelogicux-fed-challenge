e210f292469bcd4794385028ff196ca9c91ee711
David Brown <davidbrown@corelogic.com>
2022-06-24 12:26:49 -0700

    added art detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ArtifactDetailComponent } from './artifact-detail/artifact-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ArtifactDetailComponent
   ],
   imports: [
     BrowserModule,
