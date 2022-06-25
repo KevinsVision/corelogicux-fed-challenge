0f76667ce00c2ebaa2c0bdf8e6a22c705f9a0e23
David Brown <davidbrown@corelogic.com>
2022-06-24 17:27:43 -0700

    getting data from the api

--- src/app/artifact-detail/artifact-detail.component.ts
+++ src/app/artifact-detail/artifact-detail.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {ArtifactInfoApiService} from 'src/app/artifact-info-api.service';
+
 
 @Component({
   selector: 'app-artifact-detail',
   templateUrl: './artifact-detail.component.html',
   styleUrls: ['./artifact-detail.component.scss']
 })
+
+
 export class ArtifactDetailComponent implements OnInit {
 
-  constructor() { }
+  artifactData: any = [];
+
+  constructor(private api: ArtifactInfoApiService) {
+  }
 
   ngOnInit(): void {
-  }
+
+    this.api.getArtifactDetail().subscribe((...data) =>
+      this.artifactData = JSON.parse(JSON.stringify(data)));
+  };
 
 }
