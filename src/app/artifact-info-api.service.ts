aabe3f6b67dedfd1c768e647dbf2be61663fee4e
David Brown <davidbrown@corelogic.com>
2022-06-28 14:06:25 -0700

    art detail component created

--- src/app/artifact-info-api.service.ts
+++ src/app/artifact-info-api.service.ts
@@ -8,10 +8,21 @@ import {catchError, retry} from 'rxjs/operators';
 })
 export class ArtifactInfoApiService {
 
+  // apiURL: string = 'https://api.artic.edu/api/v1/artworks/120081';
+
   constructor(private http: HttpClient) {
   }
 
   getArtifactDetail() {
     return this.http.get('https://api.artic.edu/api/v1/artworks/120081');
   }
+
+  // getArtifactDetailV(): Observable<any> {
+  //   return this.http.get<any>(this.apiURL);
+  // }
+
+
 }
+
+
+
