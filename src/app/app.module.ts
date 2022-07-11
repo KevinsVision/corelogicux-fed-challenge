d7c77285eda1e91e27f967b78b4c7eeddf6ba070
David Brown <davidbrown@corelogic.com>
2022-07-11 16:44:24 -0700

    newsletter added still need to do form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from "@angular/forms";
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {ArtifactDetailComponent} from './artifact-detail/artifact-detail.component';
-import { ArtifactListingComponent } from './artifact-listing/artifact-listing.component';
+import {ArtifactListingComponent} from './artifact-listing/artifact-listing.component';
+import {NewsletterFormComponent} from './newsletter-form/newsletter-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ArtifactDetailComponent,
-    ArtifactListingComponent
+    ArtifactListingComponent,
+    NewsletterFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/newsletter-form/newsletter-form.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-newsletter-form',
+  templateUrl: './newsletter-form.component.html',
+  styleUrls: ['./newsletter-form.component.scss']
+})
+export class NewsletterFormComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+}
