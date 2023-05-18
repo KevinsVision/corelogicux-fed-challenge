748e0cfc91e99d9d57ffb7bcead6ac2dc4f4fee6
Kevin Lopez Cifuentes <US.Kevin@icloud.com>
2023-05-18 15:59:44 -0700

    interview execerise with Nathan

--- src/app/artifact-listing/artifact-listing.component.ts
+++ src/app/artifact-listing/artifact-listing.component.ts
@@ -1,14 +1,25 @@
 import {Component, OnInit} from '@angular/core';
+import { ArtifactInfoApiService } from '../artifact-info-api.service';
 
 @Component({
   selector: 'app-artifact-listing',
   templateUrl: './artifact-listing.component.html',
   styleUrls: ['./artifact-listing.component.scss']
 })
-export class ArtifactListingComponent {
+export class ArtifactListingComponent implements OnInit {
 
-  constructor() {
+  artifactListing: any = [];
+  artifactData: any = [];
 
+  constructor(private api: ArtifactInfoApiService) {
+  }
+
+  ngOnInit(): void {
+    this.api.getArtifactListings().subscribe((...data) =>
+      this.artifactListing = JSON.parse(JSON.stringify(data)));
+
+      this.api.getArtifactDetail().subscribe((...data) =>
+      this.artifactData = JSON.parse(JSON.stringify(data)));
   }
 
 }
